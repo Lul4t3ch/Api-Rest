93b0005994f83a3e0ca9908edf2f68dc2bbee062
Lul4t3ch <lul4t3ch15@gmail.com>
2023-05-14 16:01:55 -0300

    Lista os alunos com suas fotos

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -1,8 +1,16 @@
 import Aluno from '../models/Aluno';
+import Foto from '../models/Foto';
 
 class AlunoController {
   async index(req, res) {
-    const alunos = await Aluno.findAll();
+    const alunos = await Aluno.findAll({
+      attributes: ['id', 'nome', 'sobrenome', 'email', 'idade', 'peso', 'altura'],
+      order: [['id', 'DESC'], [Foto, 'id', 'DESC']],
+      include: {
+        model: Foto,
+        attributes: ['filename'],
+      },
+    });
     res.json(alunos);
   }
 
@@ -55,7 +63,17 @@ class AlunoController {
         });
       }
 
-      const aluno = await Aluno.findByPk(id);
+      const aluno = await Aluno.findByPk(
+        id,
+        {
+          attributes: ['id', 'nome', 'sobrenome', 'email', 'idade', 'peso', 'altura'],
+          order: [['id', 'DESC'], [Foto, 'id', 'DESC']],
+          include: {
+            model: Foto,
+            attributes: ['filename'],
+          },
+        },
+      );
 
       if (!aluno) {
         return res.status(400).json({
@@ -101,12 +119,3 @@ class AlunoController {
 }
 
 export default new AlunoController();
-
-// const novoAluno = await Aluno.create({
-//   nome: 'maria',
-//   sobrenome: 'Henrique',
-//   email: 'mariasantosmatos15@gmail.com',
-//   idade: 32,
-//   peso: 80,
-//   altura: 1.70,
-// });
